b768af28572f944aa2c09774ffab1f2ee1fad658
kojix2 <2xijok@gmail.com>
2025-06-21 17:30:43 +0900

    Refactor integration tests to ensure binary is built before execution

--- spec/kc_spec.cr
+++ spec/kc_spec.cr
@@ -2,6 +2,12 @@ require "./spec_helper"
 require "file_utils"
 
 describe "kc integration" do
+  before_all do
+    unless File.exists?("./bin/kc")
+      system("shards build --release -Dpreview_mt -Dexecution_context")
+    end
+  end
+
   it "runs without errors" do
     File.tempfile("test", ".fastq") do |file|
       file.puts "@read1"
@@ -9,9 +15,9 @@ describe "kc integration" do
       file.puts "+"
       file.puts "IIII"
       file.flush
-
+      
       result = `./bin/kc -i #{file.path} 2>/dev/null`
-
+      
       result.should contain("ID")
       result.should contain("read1")
     end
