ac9a09934e5e46f404cb78d5da0a035c0cdc0917
kojix2 <2xijok@gmail.com>
2025-06-21 17:21:27 +0900

    Add CI build workflow and implement k-mer counting functionality with tests

--- spec/kc_spec.cr
+++ spec/kc_spec.cr
@@ -1,9 +1,29 @@
 require "./spec_helper"
+require "file_utils"
 
-describe Kc do
-  # TODO: Write tests
+describe "kc integration" do
+  it "runs without errors" do
+    File.tempfile("test", ".fastq") do |file|
+      file.puts "@read1"
+      file.puts "ATCG"
+      file.puts "+"
+      file.puts "IIII"
+      file.flush
 
-  it "works" do
-    false.should eq(true)
+      result = `./bin/kc -i #{file.path} 2>/dev/null`
+
+      result.should contain("ID")
+      result.should contain("read1")
+    end
+  end
+
+  it "shows help" do
+    result = `./bin/kc --help 2>&1`
+    result.should contain("Usage: kc")
+  end
+
+  it "shows error for missing input" do
+    result = `./bin/kc 2>&1`
+    result.should contain("Input file is required")
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,3 @@
 require "spec"
-require "../src/kc"
+# Note: We don't require the main kc.cr file directly since it's an executable
+# Instead, we test the compiled binary through shell commands
