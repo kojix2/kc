fed570b85beb028073cd95c2f57a0707f173320c
kojix2 <2xijok@gmail.com>
2025-06-21 23:23:00 +0900

    Add ArrowSparse module

--- Makefile
+++ Makefile
@@ -1,46 +1,22 @@
-# Makefile for building the Arrow sparse static library and Crystal application
-
-# Compiler settings
-CXX = g++
-CXXFLAGS = -std=c++11 -fPIC -O2 -Wall
-
-# Library settings
-LIB_NAME = libarrow_sparse
-STATIC_LIB = $(LIB_NAME).a
-
-# Source files
-CPP_SOURCES = src/arrow_sparse.cpp
-CPP_OBJECTS = $(CPP_SOURCES:.cpp=.o)
+# Makefile for building Crystal application
 
 # Crystal application
 CRYSTAL_APP = kc
 CRYSTAL_SOURCE = src/kc.cr
 CRYSTAL_FLAGS = -Dpreview_mt -Dexecution_context
 
-.PHONY: all clean install test
+.PHONY: all clean test
 
-# Default target: build static library
-all: $(STATIC_LIB)
+# Default target: build Crystal application
+all: $(CRYSTAL_APP)
 
-# Build static library
-$(STATIC_LIB): $(CPP_OBJECTS)
-	ar rcs $@ $^
-
-# Compile C++ object files
-%.o: %.cpp
-	$(CXX) $(CXXFLAGS) -c $< -o $@
-
-# Build Crystal application (depends on static library)
-$(CRYSTAL_APP): $(STATIC_LIB)
+# Build Crystal application
+$(CRYSTAL_APP):
 	crystal build $(CRYSTAL_SOURCE) $(CRYSTAL_FLAGS) -o $@
 
 # Clean build artifacts
 clean:
-	rm -f $(CPP_OBJECTS) $(STATIC_LIB) $(CRYSTAL_APP)
-
-# Install static library to system
-install: $(STATIC_LIB)
-	cp $(STATIC_LIB) /usr/local/lib/ || sudo cp $(STATIC_LIB) /usr/local/lib/
+	rm -f $(CRYSTAL_APP)
 
 # Test: build Crystal application
 test: $(CRYSTAL_APP)
--- src/arrow_writer.cr
+++ src/arrow_writer.cr
@@ -1,8 +1,4 @@
-# Arrow sparse tensor bindings
-@[Link(ldflags: "#{__DIR__}/../libarrow_sparse.a -lstdc++")]
-lib ArrowSparse
-  fun write_arrow_sparse(filename : LibC::Char*, coords : LibC::Int64T*, values : LibC::UInt32T*, read_names : LibC::Char**, read_name_lengths : LibC::Int64T*, nnz : LibC::Int64T, num_rows : LibC::Int64T, num_cols : LibC::Int64T) : LibC::Int
-end
+require "./arrow_sparse"
 
 module ArrowWriter
   def self.write_sparse_coo(filename : String, data : Array({String, String, UInt32}), all_kmers : Array(String))
@@ -16,39 +12,23 @@ module ArrowWriter
     read_ids.each_with_index { |read_id, idx| read_to_row[read_id] = idx.to_i64 }
 
     # Prepare coordinate and value arrays
-    coords = LibC.malloc(data.size * 2 * sizeof(Int64)).as(Int64*)
-    values = LibC.malloc(data.size * sizeof(UInt32)).as(UInt32*)
+    coords = Array(Int64).new(data.size * 2)
+    values = Array(UInt32).new(data.size)
 
-    data.each_with_index do |(read_id, kmer, count), i|
+    data.each do |(read_id, kmer, count)|
       row = read_to_row[read_id]
       col = kmer_to_col[kmer]
 
-      coords[i * 2] = row
-      coords[i * 2 + 1] = col
-      values[i] = count
+      coords << row
+      coords << col
+      values << count
     end
 
     num_rows = read_ids.size.to_i64
     num_cols = all_kmers.size.to_i64
     nnz = data.size.to_i64
 
-    # Use the new version with read names
-    # Prepare read names arrays
-    read_name_ptrs = LibC.malloc(read_ids.size * sizeof(Pointer(UInt8))).as(Pointer(UInt8)*)
-    read_name_lengths = LibC.malloc(read_ids.size * sizeof(Int64)).as(Int64*)
-
-    read_ids.each_with_index do |read_id, i|
-      read_name_ptrs[i] = read_id.to_unsafe
-      read_name_lengths[i] = read_id.bytesize.to_i64
-    end
-
-    result = ArrowSparse.write_arrow_sparse(filename, coords, values, read_name_ptrs, read_name_lengths, nnz, num_rows, num_cols)
-
-    LibC.free(coords.as(Void*))
-    LibC.free(values.as(Void*))
-    LibC.free(read_name_ptrs.as(Void*))
-    LibC.free(read_name_lengths.as(Void*))
-
-    result == 0
+    # Use the Crystal implementation
+    ArrowSparse.write_arrow_sparse(filename, coords, values, read_ids, nnz, num_rows, num_cols)
   end
 end
